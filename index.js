e119e90b69c4355f145a8251d2b2ce9ae847a1a5
nerdfiles <nerdfiles@gmail.com>
2016-04-25 19:40:28 -0500

    Cleanup @TODO docopt-js install.

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-/**(
- * @fileOverview ./index.js
- * @description
- * Collective Pizza Button Bot.
- * )
- */
-var five = require("johnny-five");
-var request = require("request");
+var docopt = require('docopt-js');
+var bots = require('bots');
 
-var interactionModel = function () {
-  var modelInterface = {};
-  modelInterface.loaded = ["down"];
 
-  return function () {
-    return modelInterface;
-  };
+function docParser (f) {
+  return f.toString().
+    replace(/^[^\/]+\/\*!?/, '').
+    replace(/\*\/[^\/]+$/, '');
 }
 
-new five.Board().on("ready", function() {
-  var button = new five.Button("O5");
+var doc = docParser(function() {/*!
+Usage:
+  collective order <pizza_term>
+  collective show <pizza_concept>
+  collective add <pizza_schema_url>
+  collective generate <pizza_schema>
+  collective -h | --help | --version
+*/});
 
-  interactionModel()().loaded.forEach(function(type) {
-    button.on(type, function() {
-      var purchase = request.post('/api/v1/purchase');
-      purchase.then(function (validatedPurchaseRequest) {
-        console.log(validatedPurchaseRequest);
-      });
-    });
-  });
-});
+function boot (config) {
+  var defaultOpts = {
+    'order'       : false,
+    '<pizza_term>'       : null,
+    'show'         : false,
+    '<pizza_concept>'    : null,
+    'add'          : false,
+    '<pizza_schema_url>' : null,
+    'generate'     : false,
+    '<pizza_schema>'     : null,
+    '-h'           : false,
+    '--help'       : false,
+    '--version'    : false
+  };
+  var opts = config || defaultOpts;
+  console.dir(opts);
+
+}
+
+function init () {
+  var initConfig = docopt.docopt(doc, { version: '0.0.1' });
+  boot(initConfig);
+}
+
+init();
